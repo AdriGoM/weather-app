a447c73af82bfb3597a7334f0a902f7aa86df22d
Javier Hertfelder <javier@fxstreet.com>
2021-06-11 07:12:01 +0200

    Removed example folders and improve naming

--- cypress/integration/weatherapp_basicinteractions_spec.js
+++ cypress/integration/weatherapp_basicinteractions_spec.js
@@ -4,7 +4,7 @@ context('Actions', () => {
       cy.visit('http://localhost:3000/')
     })
 
-describe('My First Test', () => {
+describe('Button shows correct information when clicked', () => {
     it('Click on the button without location and get a 400 message status', () => {
         cy.get('button.search-button').click();
         cy.contains("h2", "Request failed with status code 400");     
