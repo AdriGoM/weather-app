833ed2413333f11ca0d6963428d6a6b6db29d525
Adrián Godoy <adrian@fxstreet.com>
2021-07-20 04:28:15 +0200

    AAA Pattern and  helper restored

--- src/__tests__/unit/weather.test.tsx
+++ src/__tests__/unit/weather.test.tsx
@@ -3,73 +3,105 @@ import { toCelsius, toKilometersHour, getWeatherIcon } from '../../helpers/weath
 
 
 test('toCelsius should return kelvin temperature converted correctly', () => {
-
+  
+  // arrange
   var toConvert = 300;
-  var result = toCelsius(toConvert);
   var expected = 26;
 
+  // act
+  var result = toCelsius(toConvert);
+
+  // assert
   expect(result).toBe(expected);
 });
 
 test('toCelsius should return no data meesage if temperature is undefined', () => {
-
+  
+  // arrange
   var toConvert = undefined;
-  var result = toCelsius(toConvert);
   var expected = "No data to show!"
 
+  // act
+  var result = toCelsius(toConvert);
+
+  // assert
   expect(result).toBe(expected);
 });
 
 test('toCelsius should return invalid data type message if temperature is not a number', () => {
-
+ 
+  // arrange
   var toConvert = "300";
-  var result = toCelsius(toConvert);
   var expected = "Invalid type of data!"
 
+  // act
+  var result = toCelsius(toConvert);
+
+  // assert
   expect(result).toBe(expected);
 });
 
 test('toKilometersHour should return speed converted correctly', () => {
-
+  
+  // arrange
   var toConvert = 20;
-  var result = toKilometersHour(toConvert);
   var expected = 72;
 
+  // act
+  var result = toKilometersHour(toConvert);
+
+  // assert 
   expect(result).toBe(expected);
 });
 
 test('toKilometersHour should return no data meesage if speed is undefined', () => {
 
+  // arrange
   var toConvert = undefined;
-  var result = toKilometersHour(toConvert);
   var expected = "No data to show!"
 
+  // act
+  var result = toKilometersHour(toConvert);
+
+  // assert
   expect(result).toBe(expected);
 });
 
 test('toCelsius should return invalid data type message if speed is not a number', () => {
 
+  // arrange
   var toConvert = "300";
-  var result = toKilometersHour(toConvert);
   var expected = "Invalid type of data!"
 
+  // act
+  var result = toKilometersHour(toConvert);
+
+  // assert
   expect(result).toBe(expected);
 });
 
 test('getWeatherIcon should return thunderstorm image if weather code is between 200 and 300', () => {
-
+  
+  // arrange
   var weatherId = "200";
-  var result = getWeatherIcon(weatherId);
   var expected = "http://openweathermap.org/img/wn/11d@2x.png";
 
+  // act
+  var result = getWeatherIcon(weatherId);
+
+  // assert
   expect(result).toBe(expected);
 });
 
 test('getWeatherIcon should return clear sky image if weather code is equal to 800', () => {
 
+  // arrange
   var weatherId = "800";
-  var result = getWeatherIcon(weatherId);
   var expected = "http://openweathermap.org/img/wn/01d@2x.png";
 
+  // act
+  var result = getWeatherIcon(weatherId);
+  
+  // assert
   expect(result).toBe(expected);
 });
